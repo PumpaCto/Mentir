82b2eec41029eafdbefa3944c95c4c7d8fbb4c24
PumpaCto <pumpa6827@gmail.com>
2025-04-29 00:55:51 +0300

    Create nftData.js

---
+++ src/data/nftData.js
@@ -0,0 +1,22 @@
+export const nftData = [
+  {
+    name: "MiraVerse Guardian",
+    image: "/images/nft/nft1.png",
+  },
+  {
+    name: "Crypto Mage",
+    image: "/images/nft/nft2.png",
+  },
+  {
+    name: "Blockchain Samurai",
+    image: "/images/nft/nft3.png",
+  },
+  {
+    name: "Pumpa Pilot",
+    image: "/images/nft/nft4.png",
+  },
+  {
+    name: "AI Hero",
+    image: "/images/nft/nft5.png",
+  },
+];
