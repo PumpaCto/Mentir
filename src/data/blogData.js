3551098a95fde6eca62399c0e710ea1966026c47
PumpaCto <pumpa6827@gmail.com>
2025-04-29 01:01:06 +0300

    Create blogData.js

---
+++ src/data/blogData.js
@@ -0,0 +1,20 @@
+export const blogData = [
+  {
+    title: "MiraVerse Token Launch Confirmed!",
+    excerpt: "Get ready! MiraVerse Token is set to launch on Pump.fun. Details inside!",
+    image: "/images/blog/launch.jpg",
+    slug: "miraverse-token-launch",
+  },
+  {
+    title: "First Look at Our AI Crypto Trader Game",
+    excerpt: "Sneak peek into the upcoming AI-based game shaping crypto futures!",
+    image: "/images/blog/game-preview.jpg",
+    slug: "ai-crypto-trader-game",
+  },
+  {
+    title: "MiraVerse Anime Series Announcement",
+    excerpt: "An anime revolution is coming - MiraVerse official series release scheduled!",
+    image: "/images/blog/anime-series.jpg",
+    slug: "anime-series-announcement",
+  },
+];
