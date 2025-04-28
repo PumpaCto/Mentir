bef2e2d2bc590340fbff8e6ea29268b7385897f8
PumpaCto <pumpa6827@gmail.com>
2025-04-29 00:54:07 +0300

    Create tokenomicsData.js

---
+++ src/data/tokenomicsData.js
@@ -0,0 +1,27 @@
+export const tokenomicsData = [
+  {
+    label: "Liquidity",
+    percentage: 40,
+    color: "#00bfff",
+  },
+  {
+    label: "Community Rewards",
+    percentage: 25,
+    color: "#ff9900",
+  },
+  {
+    label: "Marketing",
+    percentage: 20,
+    color: "#ff4c4c",
+  },
+  {
+    label: "Development",
+    percentage: 10,
+    color: "#6a5acd",
+  },
+  {
+    label: "Reserve",
+    percentage: 5,
+    color: "#3cb371",
+  },
+];
