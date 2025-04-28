62441196923ef2ddb080212942190c62258d4906
PumpaCto <pumpa6827@gmail.com>
2025-04-29 00:58:41 +0300

    Create partnersData.js

---
+++ src/data/partnersData.js
@@ -0,0 +1,18 @@
+export const partnersData = [
+  {
+    name: "Red Star Animation",
+    logo: "/images/partners/redstar.png",
+  },
+  {
+    name: "Pump.fun",
+    logo: "/images/partners/pumpfun.png",
+  },
+  {
+    name: "Crypto Galaxy",
+    logo: "/images/partners/cryptogalaxy.png",
+  },
+  {
+    name: "AnimeChain",
+    logo: "/images/partners/animechain.png",
+  },
+];
