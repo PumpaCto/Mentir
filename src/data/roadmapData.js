435c821c0e3fd677ef56092d95faf96b1a777dd9
PumpaCto <pumpa6827@gmail.com>
2025-04-29 00:52:47 +0300

    Create roadmapData.js

---
+++ src/data/roadmapData.js
@@ -0,0 +1,18 @@
+export const roadmapData = [
+  {
+    quarter: "Q2 2025",
+    details: "MiraVerse Token Launch on Pump.fun & community building.",
+  },
+  {
+    quarter: "Q3 2025",
+    details: "AI-Based Choice Crypto Trader Game Beta Release.",
+  },
+  {
+    quarter: "Q4 2025",
+    details: "NFT Collections Launch + In-Game Utility Integration.",
+  },
+  {
+    quarter: "Q1 2026",
+    details: "Official Anime Series Release on Major Streaming Platform.",
+  },
+];
