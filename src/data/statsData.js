9d2142f5f1db9005b76133f46ba90219325e63bd
PumpaCto <pumpa6827@gmail.com>
2025-04-29 00:57:30 +0300

    Create statsData.js

---
+++ src/data/statsData.js
@@ -0,0 +1,14 @@
+export const statsData = [
+  {
+    number: "10K",
+    label: "Twitter Followers",
+  },
+  {
+    number: "5K",
+    label: "Telegram Members",
+  },
+  {
+    number: "1K",
+    label: "Early NFT Holders",
+  },
+];
