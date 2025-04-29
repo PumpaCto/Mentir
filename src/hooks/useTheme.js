23e08c2cd0fcf27b33d7c4076f8f6c10d65816f1
PumpaCto <pumpa6827@gmail.com>
2025-04-29 12:29:51 +0300

    Update useTheme.js

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -1,6 +1,31 @@
-import { useThemeContext } from "@/context/ThemeContext";
+"use client";
+
+import { useState, useEffect } from "react";
 
 export const useTheme = () => {
-  const { theme, toggleTheme } = useThemeContext();
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    // İlk açıldığında localStorage'dan tema kontrol et
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme) {
+      setTheme(storedTheme);
+      document.documentElement.classList.add(storedTheme);
+    } else {
+      setTheme("light");
+      document.documentElement.classList.add("light");
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "dark" ? "light" : "dark";
+
+    document.documentElement.classList.remove(theme);
+    document.documentElement.classList.add(newTheme);
+
+    localStorage.setItem("theme", newTheme);
+    setTheme(newTheme);
+  };
+
   return { theme, toggleTheme };
 };
